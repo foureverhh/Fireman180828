af1575de8fbbda72d4bad7c481065e99dd4068ca
foureverhh <foureverhh@gmail.com>
2018-08-31 12:55:24 +0200

    Collider2D.IsTouching works to detect whether fireman and jumper collides

--- Fireman180828/Assets/Scripts/GameManager.cs
+++ Fireman180828/Assets/Scripts/GameManager.cs
@@ -7,25 +7,56 @@ public class GameManager : MonoBehaviour {
     public GameObject enemy;
     public GameObject player;
     Collider2D firemanCollider;
-    Collider2D jumperCollider;
+    //Collider2D jumperCollider;
+    
+    //To controll the spawntime and moverDeley
+    public float spwanInterval = 5f;
+    public float moveDelay = 0.5f;
 
 	// Use this for initialization
 	void Start () {
-        firemanCollider = enemy.GetComponentInChildren<Collider2D>();
-        jumperCollider = player.GetComponentInChildren<Collider2D>();  
+        firemanCollider = player.GetComponentInChildren<Collider2D>();
+        //jumperCollider = enemy.GetComponentInChildren<Collider2D>();  
+        StartCoroutine(spawnJumper());
+    
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        Crack();
-	}
+	//void Update () {
+    //    Crack();
+	//}
 
-    void Crack()
-    {
-        if (firemanCollider == null || jumperCollider == null)
-            Debug.Log("Collider can not found");
-        else if (firemanCollider.IsTouching(jumperCollider))
-            Debug.Log("Two touches");
+    IEnumerator spawnJumper()
+    { 
+        while (true)
+        {
+            NewJumper(moveDelay);
+            yield return new WaitForSeconds(spwanInterval);
+        }
     }
 
+    void NewJumper(float moveDelay)
+    {
+        GameObject newJumper = Instantiate(enemy);
+        JumperController jumperController = newJumper.GetComponentInChildren<JumperController>();
+        jumperController.moveInterval = moveDelay;
+        //jumperCollider = newJumper.GetComponentInChildren<Collider2D>();
+        //To make access to gameManager in newjumper so that carry out crack() on dangerPosition in JumperController
+        newJumper.GetComponentInChildren<JumperController>().gameManager = this;
+    }
+
+    public bool Crack(GameObject jumper)
+    {
+        if(firemanCollider == null || jumper.GetComponent<Collider2D>() == null)
+        {
+            Debug.Log("Collider can not found");
+        }
+        if (firemanCollider.IsTouching(jumper.GetComponent<Collider2D>()))
+        {
+            Debug.Log("Two touches");
+            return true; 
+        }
+        else
+            return false; 
+    }
 }
--- Fireman180828/Assets/Scripts/JumperController.cs
+++ Fireman180828/Assets/Scripts/JumperController.cs
@@ -4,35 +4,60 @@ using UnityEngine;
 
 public class JumperController : MonoBehaviour
 {
+    //Collider istouching need a reaction time of a frame then gameManager is needed to wait for one frame
+    [HideInInspector]
+    public GameManager gameManager;
 
     public List<Transform> jumperPos = new List<Transform>();
     int currentPos = 0;
-    float reactionTime = 2f;
-    float moveInterval = 0.5f;
+
+    [HideInInspector]
+    public float moveInterval;
     // Use this for initialization
     void Start()
     {
-        StartCoroutine(JumperShow());
+        StartCoroutine(JumperShow(moveInterval));
     }
 
-    //Give player 2 seconds to prepare and jumper to jump every 0.5 second
-    IEnumerator JumperShow()
+    //jumper to jump every moveInterval seconds
+    IEnumerator JumperShow(float moveInterval)
     {
-        yield return new WaitForSeconds(reactionTime);
         while (true) {
             yield return new WaitForSeconds(moveInterval);
             transform.position = jumperPos[currentPos].position;
-            JumperMove();
+           // yield return new WaitForSeconds(moveInterval);
+            StartCoroutine(JumperMove());
          //   Debug.Log("Position in Jumpshow" + currentPos);
        } 
     }
 
-    void JumperMove()
+    IEnumerator JumperMove()
     {
+        // if(currentPos < jumperPos.Count - 1)
+        // {
+        //     currentPos++;
+        //     transform.position = jumperPos[currentPos].position;
+        // }
+        //yield return null;
         currentPos++;
         if (currentPos > jumperPos.Count - 1)
+        {
+            //When jumper finished all positions, then destroy its parent
+            Destroy(transform.parent.gameObject);
             currentPos = 0;
+        }
         transform.position = jumperPos[currentPos].position;
-       // Debug.Log("Position in JumperMove" + currentPos);
+        //Give one frame to get the result of gameManager.Crack()
+        yield return null;
+        if (jumperPos[currentPos].GetComponent<DangerPosition>().danger)
+        {
+            if (gameManager.Crack(gameObject))
+            {
+                Debug.Log("Get jumper");
+            }
+            else
+                Debug.Log("Lose jumper");
+        }
+        // Debug.Log("Position in JumperMove" + currentPos);
     }
 }
